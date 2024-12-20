2aaf06c990024af7cf1214ee4cb7481289bfcd8e
ycnugroho <162649196+ycnugroho@users.noreply.github.com>
2024-12-20 20:53:47 +0700

    Update AIPlayer.java

--- TicTacToe/AIPlayer.java
+++ TicTacToe/AIPlayer.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231030 - Jonathan Abimanyu Trisno
+ * 2 - 5026231032 - Yokanan Prawira Nugroho
+ * 3 - 5026231133 - Muhammad Rifqi Alfareza Santosa
+ */
+
 package TicTacToe;
 
 /**
