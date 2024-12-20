54d9628acbbac5559575f31b609c8582c151b718
ycnugroho <162649196+ycnugroho@users.noreply.github.com>
2024-12-20 21:04:54 +0700

    Update AIPlayer.java

--- TicTacToe/AIPlayer.java
+++ TicTacToe/AIPlayer.java
@@ -10,17 +10,6 @@
 
 package TicTacToe;
 
-/**
- * Abstract superclass for all AI players with different strategies.
- * To construct an AI player:
- * 1. Construct an instance (of its subclass) with the game Board
- * 2. Call setSeed() to set the computer's seed
- * 3. Call move() which returns the next move in an int[2] array of {row, col}.
- *
- * The implementation subclasses need to override abstract method move().
- * They shall not modify Cell[][], i.e., no side effect expected.
- * Assume that next move is available, i.e., not game-over yet.
- */
 public abstract class AIPlayer {
     protected int ROWS;  // number of rows
     protected int COLS;  // number of columns
